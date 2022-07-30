005c58c7b71a6d1308d7caccdf4a140af88f3099
Mally13 <mallymutuku13@gmail.com>
2022-07-30 03:13:11 +0300

    listings

--- backend/listings/admin.py
+++ backend/listings/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Category
+from .models import SubCategory
+from .models import Product
+from .models import Shop
+from .models import Sale_Detail
+from .models import ProductImage
+
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=('name','slug','thumbnail')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display =('name', 'category', 'slug')
+    list_filter = ('category',)
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display =('name', 'subcategory', 'brand', 'slug')
+    list_filter = ('subcategory','brand')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display =('product','image')
+    list_filter = ('product',)
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display =('name',)
+
+@admin.register(Sale_Detail)
+class SaleDetailAdmin(admin.ModelAdmin):
+    list_display =('product', 'shop', 'price', 'link_to_shop','available')
+    list_filter = ('shop','product','available')
+
+
--- backend/listings/models.py
+++ backend/listings/models.py
@@ -14,29 +14,99 @@ class Category(models.Model):
 
     class Meta:
         ordering=('-name',)
+        verbose_name_plural="categories"
 
-class Product(models.Model):
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
     category=models.ForeignKey(
         Category,
-        related_name='products'
+        related_name='subcategories',
         on_delete=models.CASCADE
     )
-    name=models.CharField(max_length=100, db_index=True)
-    prices_links_jumia=models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    prices_links_konga=models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    prices_links_amazon=models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    name=models.CharField(max_length=100,unique=True)
+    slug= models.SlugField(
+        max_length=100,
+        unique=True
+    )
+        
+    class Meta:
+        ordering=('-name',)
+        verbose_name_plural="subcategories"
 
- 
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    subcategory=models.ForeignKey(
+        SubCategory,
+        related_name='products',
+        on_delete=models.CASCADE
+    )
     
+    name=models.CharField(max_length=100, unique=True)
+    brand=models.CharField(max_length=100)
+    slug= models.SlugField(max_length=100,unique=True)
+    description=models.TextField()
+    created_on=models.DateField()
+    modified_on=models.DateField()
 
-    images=JSONField(encoder=None)
-    description(
+    class Meta:
+        ordering=('-brand',)
+        verbose_name_plural='products'
+    
+    def __str__(self):
+        return self.name
 
+class ProductImage(models.Model):
+    product=models.ForeignKey(
+        Product,
+        on_delete=models.SET_NULL, null=True,blank=True
+    )    
+    image=models.ImageField(
+        upload_to="productimages/"
     )
-    links_to_shop(
+    class Meta:
+        ordering=('-product',)
+        verbose_name_plural='productimages'
 
-    )
-    available(
 
+
+class Shop(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True
     )
+    class Meta:
+        ordering=('-name',)
+        verbose_name_plural='Shops'
+    
+    def __str__(self):
+        return self.name
+
+
+class Sale_Detail(models.Model):
+    product=models.ForeignKey(
+        Product,
+        related_name='product',
+        on_delete=models.CASCADE
+    )
+    shop=models.ForeignKey(
+        Shop,
+        related_name="shop",
+        on_delete=models.CASCADE
+    )
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+    link_to_shop=models.URLField(max_length=250)
+    available=models.BooleanField(default=True)
+    
+    class Meta:
+        ordering=('price',)
+        verbose_name_plural='sale_details'
+    
+    def __str__(self):
+        return self.price
 
